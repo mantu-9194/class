540f55fe2b8f7c81dd1f2627694826794830b9cc
Mahantesh kumar <97968290+mantu-9194@users.noreply.github.com>
2022-03-04 13:30:28 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,114 @@
+//decision making
+//one condition
+/*let a = 10
+if (a == 10) {
+
+} else {
+
+}*/
+
+
+//if else ladder
+//two condition
+let a = -10;
+if (a < 0) {
+    console.log("negetive");
+} else if (a == 0) {
+    console.log("zero");
+} else {
+    console.log("positive");
+}
+
+
+//switch case
+//three or more
+
+let age=25;
+
+
+switch (true) {
+    
+    case (age<18):
+        console.log("child");
+        break;
+    case (18<age>40):
+        console.log("adult");
+        break;
+    default :
+         
+        console.log("old age");
+        break;
+}
+
+
+
+
+
+//age<18=Child
+//greater than 18 but lessthan 40=adult
+//age is greater than 40=old age
+let b=3;
+switch (b) {
+    
+    case 0:
+        console.log("monday");
+        break;
+    case 1:
+        console.log("tuesday");
+        break;
+    case 2:
+         
+        console.log("wednesday");
+        break;
+    case 3:
+         
+        console.log("thursday");
+        break;
+    case 4:
+         
+        console.log("friday");
+        break;
+    case 5:
+        console.log("saturday");
+
+        break;
+    default:
+        console.log("NA")    
+
+
+}
+//loops for while ,do ,while
+//itteration statements...
+
+//console.log(2);
+//console.log(1);
+//console.log(3);
+//console.log(4);
+//console.log(5);
+//console.log(6);
+//console.log(7);
+//console.log(8);
+//console.log(9);
+//console.log(10);
+
+//3 values
+//initialize
+//condition
+//increment/decrement
+
+//for( let i = 1; i<=10;i++ ){
+  //  console.log(i)
+   
+//}
+
+//var a; 
+
+//var a=12;  
+//we r initailsing the variable with value 12
+
+for (let i = 1; i <= 10; i++) {
+    console.log("19*" + i + "="+ 19*i);;
+  }
+
+
+
